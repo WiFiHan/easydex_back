4dfd4f7e8b9cc476afa27ca2d6a5f5a550e6b2b9
isuh88 <isuh88@snu.ac.kr>
2023-07-11 18:28:35 +0900

    [fix] model name fixed

--- django/index/models.py
+++ django/index/models.py
@@ -2,8 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Index(models.Model):
-    title = models.CharField(max_length=256)
-    content = models.TextField()
+    name = models.CharField(max_length=100, default="Dow Jones")
+    closing = models.CharField(max_length=100, default="0.00", blank=True, null=True)
+    opening = models.CharField(max_length=100, default="0.00", blank=True, null=True)
 
     def __str__(self):
         return self.title
