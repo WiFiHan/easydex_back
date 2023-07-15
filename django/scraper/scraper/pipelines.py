58ee8a1a7ab08b7608796d17576521b2bf95e8dc
isuh88 <isuh88@snu.ac.kr>
2023-07-15 19:56:29 +0900

    [feat] spider divided into getting closing and getting historical values

--- django/scraper/scraper/pipelines.py
+++ django/scraper/scraper/pipelines.py
@@ -6,12 +6,30 @@
 
 # useful for handling different item types with a single interface
 from dexmanager.models import SrcDex
+from django.core.exceptions import ObjectDoesNotExist
 
 class IndexPipeline:
     def process_item(self, item, spider):
-        index = SrcDex(title=item['title'], closing=item['closing'])
-        try:
-            index.save()
-        except Exception as e:
-            print("Error saving index:", e)
+        caller_spider = spider.name
+        if caller_spider == 'indicesinfo':
+            try: 
+                index = SrcDex.objects.get(title=item['title'])  # get existing one
+            except ObjectDoesNotExist:
+                index = SrcDex(title=item['title'])              # create new one
+            try:
+                index.closing = item['closing']
+                index.save()
+            except Exception as e:
+                print("Error saving index info:", e)
+        else:
+            try:
+                index = SrcDex.objects.get(title=item['title'])
+            except Exception as e:
+                print("Error getting index info:", e)
+            try:
+                print("Saving index values:", item['values'])
+                index.values = item['values']
+                index.save()
+            except Exception as e:
+                print("Error saving index values:", e)
         return item
