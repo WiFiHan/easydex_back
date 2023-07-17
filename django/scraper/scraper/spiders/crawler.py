1dde71beeb7577c0ebad99ccba51639db165aa80
WiFiHan <mukss0909056@gmail.com>
2023-07-17 13:52:31 +0900

    [feat]userDex Logic is complemented

--- django/scraper/scraper/spiders/crawler.py
+++ django/scraper/scraper/spiders/crawler.py
@@ -29,11 +29,14 @@ class IndicesInfoSpider(scrapy.Spider):
 
 class IndexHistorySpider(scrapy.Spider):
     name = "indexhistory"
-    
     def start_requests(self):
+        # print(self.__dict__.keys())
+        # print(self.__dict__.values())
         yield scrapy.Request(f"{self.URL}-historical-data", self.parse)
 
     def parse(self, response):
+        # 코드 실행
+        # 에러가 발생할 수 있는 작업 수행
         if ("crypto" in response.url) or ("currencies" in response.url):
             title = response.xpath('//*[@id="__next"]/div[2]/div/div/div[2]/main/div/div[1]/div[1]/h1/text()').get()
             data = response.xpath('//*[@id="__next"]/div[2]/div/div/div[2]/main/div/div[4]/div/div[1]/div/div[3]/div/table/tbody//tr')
@@ -45,4 +48,4 @@ class IndexHistorySpider(scrapy.Spider):
             date = row.xpath('td[1]/time//text()').get()
             price = row.xpath('td[2]//text()').get()
             values[date] = price
-        yield SrcDexItem(title=title, values=values)
+        yield SrcDexItem(title=title, values=values).save()
